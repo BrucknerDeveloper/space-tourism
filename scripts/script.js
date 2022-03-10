59272f689024269e3c569f637ad4dbe323747939
d.bruckner <d.bruckner@htlkrems.at>
2022-03-10 12:18:38 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
 // NAVIGATION
 
-const primaryNavigation = document.getElementById('primary-navigation');
+const primaryNav = document.getElementById('primary-navigation');
+const primaryNavLinks = document.querySelectorAll('.primary-navigation a')
 const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
 
+console.log(primaryNavLinks)
+
 mobileNavToggle.addEventListener('click', () => {
-    primaryNavigation.classList.toggle('primary-nav-active');
+    primaryNav.classList.toggle('primary-nav-active');
     mobileNavToggle.classList.toggle('mobile-nav-active');
 })
